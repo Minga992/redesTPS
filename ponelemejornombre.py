5d43db7985e9ecdbe915de5ac569b920ba0a5303
Minga992 <minga_titere@hotmail.com>
2016-09-16 23:42:57 -0300

    arreglado broadcast

--- ponelemejornombre.py
+++ ponelemejornombre.py
@@ -5,32 +5,28 @@ from scapy.all import *
 
 total_pkts = 0
 pkts_broad = 0
-ruta_archivo = sys.argv[1]
+archi = open('bradcast_fuente.txt' ,'a')
 
 def clasificar_pkt(pkt):
 		#dado un paquete printeo si es broadcast o unicast, pongo su dst , e incremento el contador correspondiente
 
+  global archi
   if (pkt.dst == "ff:ff:ff:ff:ff:ff") :
-    global pkts_broad
-    print "Broadcast: " + pkt.dst
+    global pkts_broad    
+    archi.write('Broadcast: ' + str(pkt.dst) + '\n')
     pkts_broad += 1
     
   else :
-    print "Unicast: " + pkt.dst
+    archi.write('Unicast: ' + str(pkt.dst) + '\n')
 
       
 def procesar_pkt(pkt):
 		#dado un paquete, lo clasifica en Broad o Uni con lo que esto conlleve y printea la frecuencia relativa hasta el
   	#momento de ambas clasificaciones
 		
-  global total_pkts
-    
+  global total_pkts    
   clasificar_pkt(pkt)
-
   total_pkts += 1
-  print "Frecuencia relativa de paquetes broadcast: " + str(pkts_broad) + "/" + str(total_pkts) + " - " + str(pkts_broad / total_pkts)
-  print "Frecuencia relativa de paquetes unicast: " + str(total_pkts - pkts_broad) + "/" + str(total_pkts) + " - " + str((total_pkts - pkts_broad) / total_pkts)
-
 
 def monitor_callback(pkt):
     #dado un pkt, lo escribo en un .cap, y lo proceso.
@@ -49,21 +45,30 @@ def leer_y_procesar_pcap(ruta_archivo):
   for pkt in lista_pkts:
     procesar_pkt(pkt)	
 
+  global total_pkts
+  global pkts_broad
+  global archi
+  archi.write('Frecuencia relativa de paquetes broadcast: '+ str(pkts_broad) + '/' + str(total_pkts) + ' - ' + str(pkts_broad / float(total_pkts)) + '\n')
+  archi.write('Frecuencia relativa de paquetes unicast: ' + str(total_pkts - pkts_broad) + "/" + str(total_pkts) + " - " + str((total_pkts - pkts_broad) / float(total_pkts)) + '\n')
+
 if __name__ == '__main__' :
 	#aca leo y hago cosas
+
+  ruta_archivo = sys.argv[1]
   if sys.argv[2] == "r" :
     leer_y_procesar_pcap(ruta_archivo)
-
+    archi.close()
   #aca sniffeo con scapy, y hago cosas
   elif sys.argv[2] == "w" :
+    segundosTimeOut = int(sys.argv[3])
     pktdump = PcapWriter(ruta_archivo, append=True, sync=True)
-    sniff(prn = monitor_callback , store = 0)
+    sniff(prn = monitor_callback , store = 0, timeout = segundosTimeOut)
 
+    
+    archi.write('Frecuencia relativa de paquetes broadcast: '+ str(pkts_broad) + '/' + str(total_pkts) + ' - ' + str(pkts_broad / float(total_pkts)) + '\n')
+    archi.write('Frecuencia relativa de paquetes unicast: ' + str(total_pkts - pkts_broad) + '/' + str(total_pkts) + ' - ' + str((total_pkts - pkts_broad) / float(total_pkts)) + '\n')
+
+    archi.close()
   #aca alguien metio la pata
   else :
     raise ValueError('El segundo argumento debe ser r para leer un archivo, o w para crear uno')
-    
-
-
-
-
