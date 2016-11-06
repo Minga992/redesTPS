6567c5c71428180544d7511673feac7fb7fedcd5
Matias Guerson <matias.guerson@gmail.com>
2016-11-06 11:11:16 -0300

    si se alcanzó la ip destino no se busca la ip por mayor cantidad de respuestas, sino que ya se toma la de destino

--- TP2/tp2.py
+++ TP2/tp2.py
@@ -4,18 +4,17 @@
 import sys
 from scapy.all import *
 
-def process(hostname):
+def process(hostname, max_ttls, ttl_repetitions):
 
+	# hostname : Host destino al que se quiere alcanzar
+	# max_ttls: Valor máximo de ttl (si el paquete nunca alcanza el destino puede quedar flotando de por vida si no)
+	# ttl_repetitions: Por cada ttl enviamos esta cantidad de paquetes ya que las rutas pueden ir variando.
+	# Luego, por cada ttl, nos quedaremos con la ip que más veces respondió en estas repeticiones
+
+	
 	# Constantes de tipos de paquetes
 	TYPE_ECHO_REPLY = 0
 	TYPE_ECHO = 8
-
-	# Valor máximo de ttl (si el paquete nunca alcanza el destino puede quedar flotando de por vida si no)
-	MAX_TTLS = int(sys.argv[2])
-	
-	# Por cada ttl enviamos esta cantidad de paquetes ya que las rutas pueden ir variando.
-	# Luego, por cada ttl, nos quedaremos con la ip que más veces respondió en estas repeticiones
-	TTL_REPETITIONS = int(sys.argv[3])
 
 	# Booleano para determinar si ya alcanzamos el destino, en tal caso dejamos de procesar
 	host_reached = False
@@ -26,9 +25,9 @@ def process(hostname):
 	# Último rtt promedio calculado, para calcular el rtt del nuevo hop
 	last_rtt_avg = 0
 
-	print "{0:3s} {1:15s} {2:8s}".format("ttl", "ip", "rtt (ms)")
+	print "{0:3s}\t{1:15s}\t{2:8s}".format("ttl", "ip", "rtt (ms)")
 
-	while(not host_reached and ttl < MAX_TTLS):
+	while(not host_reached and ttl < max_ttls):
 
 		# Diccionario en donde guardamos por cada ip que responde, un listado de los rtt
 		ips_rtts = {}
@@ -36,7 +35,10 @@ def process(hostname):
 		# Diccionario en donde guardamos por cada ip que responde, el response type (para luego saber si es el host destino)
 		ips_responseType = {}
 
-		for repetition in range(TTL_REPETITIONS):
+		# Ip seleccionada por cada ttl de entre todas las que responden
+		selected_ip = None
+
+		for repetition in range(ttl_repetitions):
 
 			# Se genera y envía el paquete con el valor actual del ttl
 			packet = IP(dst=hostname, ttl=ttl)/ ICMP(type=TYPE_ECHO)
@@ -64,19 +66,24 @@ def process(hostname):
 			ips_rtts.setdefault(response.src, [])
 			ips_rtts[response.src].append(rtt)
 
-			ips_responseType[response.src] = response.type
+			if(response.type == TYPE_ECHO_REPLY):
+				host_reached = True
+				selected_ip = response.src
+				break
 
 
 		if ips_rtts:
-			# Como la idea consiste en quedarse, para cada valor del ttl, con la ip que más veces respondió, 
-			# recorremos el diccionario y nos quedamos con esa ip
-			max_responses = 0
-			selected_ip = None
 
-			for ip, rtts in ips_rtts.iteritems():
-				if(len(rtts) >= max_responses):
-					selected_ip = ip
-					max_responses = len(rtts)
+			if not host_reached:
+				# Como la idea consiste en quedarse, para cada valor del ttl, con la ip que más veces respondió, 
+				# recorremos el diccionario y nos quedamos con esa ip. En caso de que alguna de las que haya respondido
+				# sea la ip del destino, entonces previamente se tomo esa
+				max_responses = 0
+
+				for ip, rtts in ips_rtts.iteritems():
+					if(len(rtts) >= max_responses):
+						selected_ip = ip
+						max_responses = len(rtts)
 
 
 			# Luego, para la ip que más veces respondió, calculamos el rtt promedio y le restamos el rtt promedio del
@@ -85,11 +92,7 @@ def process(hostname):
 			rtt_hop = max(0, rtt_avg - last_rtt_avg)
 			last_rtt_avg = rtt_avg
 
-			print "{0} {1} {2:f}".format(ttl, selected_ip, rtt_hop * 1000.0)
-
-			# Si se alcanzó el destino, seteamos la variable host_reached en True para que deje de iterar
-			if(ips_responseType[selected_ip] == TYPE_ECHO_REPLY):
-				host_reached = True
+			print "{0}\t{1}\t{2:f}".format(ttl, selected_ip, rtt_hop * 1000.0)
 
 		ttl += 1
 
@@ -99,4 +102,4 @@ if __name__ == '__main__':
 	if(len(sys.argv) != 4):
 		print "La cantidad de parámetros es incorrecta: \n\n python tp2.py -hostname -max_ttls -ttl_repetitions \n\n hostname: es la URL o la IP del host al cual se intenta hacer Traceroute.\n max_ttls: es la cantidad máxima de TTLs hasta la que se va a llegar a probar.\n ttl_repetitions: es la cantidad de paquetes que se enviarán en ráfaga para cada TTL que se pruebe."
 	else:
-		process(sys.argv[1])
+		process(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
